42361f8be7938ea841adebaaf123e679b56e6853
Nick Minchenkov <sunty1@mail.ru>
2020-05-03 13:20:23 +0300

    Add alias @ to src folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import React from 'react';
-import Routes from './Routes/Routes'
+import React from 'react'
+import Routes from '@/Routes/Routes'
 
 function App() {
-  return (
-    <Routes className="routes"/>
-  );
+  return <Routes className='routes' />
 }
 
-export default App;
+export default App
