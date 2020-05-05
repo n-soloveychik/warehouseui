455ccd67ea15dbc7dbefbe1c296d9345ea01ecec
Nick Minchenkov <sunty1@mail.ru>
2020-05-05 22:46:36 +0300

    Make CList component stateless

--- src/components/Lists/CList/CList.js
+++ src/components/Lists/CList/CList.js
@@ -1,34 +1,32 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { List, ListItem, Typography } from '@material-ui/core'
 import classes from './CList.module.scss'
 
-class CList extends Component {
-  render() {
-    const items = this.props.items.map((item, index) => {
-      const className = [classes.item]
-      if (this.props.currentItem === item) {
-        className.push(classes['item--current'])
-      }
-      return (
-        <ListItem
-          onClick={() => this.props.handleItemClick(item)}
-          className={className.join(' ')}
-          key={index}
-        >
-          {item}
-        </ListItem>
-      )
-    })
-
+const CList = (props) => {
+  const items = props.items.map((item, index) => {
+    const className = [classes.item]
+    if (props.currentItem === item) {
+      className.push(classes['item--current'])
+    }
     return (
-      <>
-        <Typography variant='h5' style={{ marginBottom: 16 }}>
-          {this.props.title}
-        </Typography>
-        <List className={classes.paper}>{items}</List>
-      </>
+      <ListItem
+        onClick={() => props.handleItemClick(item)}
+        className={className.join(' ')}
+        key={index}
+      >
+        {item}
+      </ListItem>
     )
-  }
+  })
+
+  return (
+    <>
+      <Typography variant='h5' style={{ marginBottom: 16 }}>
+        {props.title}
+      </Typography>
+      <List className={classes.paper}>{items}</List>
+    </>
+  )
 }
 
 export default CList
