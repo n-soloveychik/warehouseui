edff018993d000d0cfc6e6eb289e3abbea965e28
nickml <sunty1@mail.ru>
2020-06-26 13:06:52 +0300

    Make search case insensitive

--- src/redux/getters/ordersGetters.js
+++ src/redux/getters/ordersGetters.js
@@ -1,10 +1,12 @@
 export const foundOrdersGetter = (state) => {
   state = state.warehouse;
   const orders = state.orders;
-  const search = state.orderSearch;
+  const search = state.orderSearch.toLowerCase();
   return orders.filter(
     (order) =>
-      order.order_num.includes(search) ||
-      order.invoices.find((invoice) => invoice.invoice_code.includes(search))
+      order.order_num.toLowerCase().includes(search) ||
+      order.invoices.find((invoice) =>
+        invoice.invoice_code.toLowerCase().includes(search)
+      )
   );
 };
